7e5909efd4938d10cf5790d48a0dd9800b166de2
1HassanAbdi <hassanabdigaleb@gmail.com>
2021-04-08 23:29:59 -0400

    une petite modification

--- projetAssurance/projetAssurance/Program.cs
+++ projetAssurance/projetAssurance/Program.cs
@@ -9,7 +9,7 @@ namespace projetAssurance
             Etudiant e1 = new Etudiant("hassan", "abdi");
             Etudiant e2 = new Etudiant("hassan", "abdi");
             Etudiant e3 = new Etudiant("hassan", "abdi");
-
+            eETU
 
 
             Console.WriteLine(e1);
