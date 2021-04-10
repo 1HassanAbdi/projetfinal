ce3e66ab2d2f1443509ceba9ebfa95137b1532d3
1HassanAbdi <hassanabdigaleb@gmail.com>
2021-04-09 22:11:15 -0400

    je commence le test

---
+++ TESTP/TESTP1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TESTP
+{
+    public class TESTP1
+    {
+    }
+}
---
+++ TESTP/TESTP2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TESTP
+{
+    public class TESTP2
+    {
+
+    }
+}
